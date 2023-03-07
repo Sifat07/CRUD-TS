6a91b0cb058e2e5f764f6c13ade4de4c4dcce000
Sifat07 <sifat140693@gmail.com>
2023-03-08 01:53:19 +0600

    basic express server with typescript

--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,12 @@
-console.log('hello world')
+import express, { Request, Response } from "express";
+
+const app = express();
+const port = 3000;
+
+app.get('/', (req: Request, res: Response) => {
+    return res.send("Hello World")
+})
+
+app.listen(port, () => {
+    console.log("server is running on " + port)
+})
